08ba65f9382ed0f0ad91267fb590acfd9002dd79
Serge <lionheart.sergex@gmail.com>
2021-06-24 15:02:34 -0700

    Added EventListener

--- main.js
+++ main.js
@@ -14,7 +14,34 @@ const Play = () => {
     board.reset();
     let piece = new Piece(ctx);
     piece.draw();
-    board.piece=piece
-    console.table(board.grid)
+    board.piece=piece;
+    console.table(board.grid);
 }
 
+const moves = {
+    [KEY.LEFT]: p => ({...p, x: p.x-1}),
+    [KEY.RIGHT]: p => ({...p, x: p.x+1}),
+    [KEY.DOWN]: p => ({...p, y: p.y+1})
+};
+
+document.addEventListener('keydown', event =>{
+    if(moves[event.key])
+        {
+            console.log(event);
+            //Stop the event from bubbling.
+            event.preventDefault();
+
+            // Get new state of piece
+            let p =moves[event.key](board.piece);
+
+            if(board.valid(p))
+            {
+                board.piece.move(p);
+
+                // Clear old position before drawing.
+                ctx.clearRect(0,0, ctx.canvas.width, ctx.canvas.height);
+
+                board.piece.draw();
+            }
+        }
+})
