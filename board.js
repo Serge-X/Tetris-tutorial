658d86e28c3acf1c7efeadb6ff1ceaa5b1ce1b7a
Serge <lionheart.sergex@gmail.com>
2021-06-26 11:53:04 -0700

    Added function to check collision

--- board.js
+++ board.js
@@ -1,14 +1,30 @@
 class Board {
 
     // Reset the board when we start a new game.
-    reset(){
+    reset()
+    {
         this.grid=this.getEmptyBoard();
     }
 
     // Get matrix filled with zeros.
-    getEmptyBoard(){
+    getEmptyBoard()
+    {
         return Array.from(
             {length: ROWS}, () => Array(COLS).fill(0)
         );
+    }
+
+    valid(p)
+    {
+        return p.shape.every((row, dy)=>{
+            return row.every((value, dx)=>{
+                let x =p.x +dx;
+                let y =p.y +dy;
+                return (
+                    this.isEmpty(value) ||
+                    (this.insideWalls(x) && this.aboveFloor(y))
+                )
+            })
+        })
     }
 }
